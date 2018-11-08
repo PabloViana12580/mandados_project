7a4f6b13e5065eadb58f04107b09d334b5c3bb97
PabloViana12580 <via16091@uvg.edu.gt>
2018-11-07 22:05:34 -0600

    First API

---
+++ src/apis/placeApi.js
@@ -0,0 +1,25 @@
+
+const savePlace = (
+	mandado_inplace,
+	visited_by,
+	name,
+	date,
+	adress,
+) => fetch('http://localhost:8000/api/v1/lugar/' , {
+	method: 'POST',
+	headers: {
+		'Content-Type': 'application/json',
+	},
+	body: JSON.stringify({
+		mandado_inplace,
+		visited_by,
+		name,
+		date,
+		adress,
+  }),
+}).then(resultado => resultado)
+	.catch((/*Error */) =>{
+		//Do something
+	});
+
+export default savePlace;
